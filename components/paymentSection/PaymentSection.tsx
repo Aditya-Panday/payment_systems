dc8343be345fd9080795bebc9e4a81493d2ad585
Aditya9899 <aditya.panday@groupbayport.com>
2026-06-17 20:56:50 +0530

    only-main-code

--- components/paymentSection/PaymentSection.tsx
+++ components/paymentSection/PaymentSection.tsx
@@ -121,7 +121,7 @@ export default function PaymentSection() {
       </div>
 
       {/* Gateway Cards Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4 px-10 pb-16 max-w-6xl mx-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 px-10 pb-16 max-w-6xl mx-auto">
         {gateways.map((gw) => (
           <PaymentCard key={gw.id} gateway={gw} />
         ))}
